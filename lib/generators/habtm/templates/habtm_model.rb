5f2be72051f7e1b4b3c46cb8dcea37c49c7eaa8b
Camilo Sánchez <info@caxtor.co>
2024-08-20 19:11:07 -0500

    upload

---
+++ lib/generators/habtm/templates/habtm_model.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class Hola < ApplicationRecord
+  # == Relationships ========================================================
+  # belongs_to :store
+  # belongs_to :status
+end
